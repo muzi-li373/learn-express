994f7b906fb2670a6341c4a5fabd7bc8f374ec2d
king <lxt373@gmail.com>
2025-09-17 19:15:40 +0800

    feat: 使用express获取get请求参数

--- route/user.js
+++ route/user.js
@@ -4,9 +4,13 @@ const router = express.Router();
 
 router.get("/a", (req, res) => {
   console.log("🚀 ~ index.js:6 ~ req:", req);
+  const { url, query } = req;
+  console.log("🚀 ~ user.js:8 ~ url:", url);
 
   res.send({
-    msg: "Hello World",
+    msg: {
+      param: query,
+    },
   });
 });
 
